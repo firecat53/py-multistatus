b9ba0cda184e6b7d7b144b6aedca67b9efe2a0a1
Scott Hansen <firecat4153@gmail.com>
2015-09-02 22:42:44 -0700

    Updated to change battery icons as charge level changes

--- plugins/battery.py
+++ plugins/battery.py
@@ -1,4 +1,4 @@
-"""Copyright 2013 by Scott Hansen <firecat4153 @gmail.com
+"""Copyright 2015 by Scott Hansen <firecat4153 @gmail.com
 
 This file is part of py-multistatus.
 
@@ -26,23 +26,37 @@ class PluginBattery(Worker):
     def __init__(self, **kwargs):
         Worker.__init__(self, **kwargs)
 
+    def _icon_select(self, capacity):
+        """Given capacity, return the appropriate battery icon
+
+        """
+        if capacity > 75:
+            icon = self.cfg.battery.batt_icon_full
+        elif capacity > 50 and capacity <= 75:
+            icon = self.cfg.battery.batt_icon_34
+        elif capacity > 25 and capacity <= 50:
+            icon = self.cfg.battery.batt_icon_12
+        elif capacity > int(self.cfg.battery.low) and capacity <= 25:
+            icon = self.cfg.battery.batt_icon_14
+        else:
+            icon = self.cfg.battery.batt_icon_empty
+        return icon
+
     def _update_data(self):
         with open(self.cfg.battery.batt_status) as f:
             status = f.readline().strip()
         with open(self.cfg.battery.batt_charge) as f:
             capacity = int(f.readline().strip())
+            icon = self._icon_select(capacity)
         if status == "Discharging" and capacity > int(self.cfg.battery.warn):
-            capacity_str = "{} {}".format(self.cfg.battery.batt_icon,
-                                          str(capacity))
+            capacity_str = "{} {}".format(icon, str(capacity))
             out = self._color_text(capacity_str, fg=self.cfg.battery.color_fg)
         elif status == "Discharging" and capacity > int(self.cfg.battery.low):
-            capacity_str = "{} {}".format(self.cfg.battery.batt_icon,
-                                          str(capacity))
+            capacity_str = "{} {}".format(icon, str(capacity))
             out = self._color_text(capacity_str, fg=self.cfg.battery.color_bg,
                                    bg=self.cfg.battery.color_fg)
         elif status == "Discharging" and capacity <= int(self.cfg.battery.low):
-            capacity_str = "{} {}".format(self.cfg.battery.batt_icon,
-                                          str(capacity))
+            capacity_str = "{} {}".format(icon, str(capacity))
             out = self._err_text(capacity_str)
         elif status == "Charging":
             capacity_str = "{} {}".format(self.cfg.battery.ac_icon,
